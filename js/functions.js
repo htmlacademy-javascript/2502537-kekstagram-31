c876ff5be45ad351451a6351e9192aa2acc0bdfc
rumforweb <ebertzz90@gmail.com>
2024-03-10 14:28:14 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,31 @@
-// *** функция проверки длины строки //
-const stringLengthControl = (string, length) => {
-  if (string.length <= length) {
-    return true;
-  } else {
-    return false;
-  }
+//Функция перевода часов в минуты
+const getHoursInMinutes = (time) => {
+  time = Number((time[0] * 60)) + Number(time[1]);
+  return time;
 };
 
-// проверки функции
-stringLengthControl('проверяемая строка', 20); // true
-stringLengthControl('проверяемая строка', 18); // true
-stringLengthControl('проверяемая строка', 10); // false
+//функция расчета попадания в рабочее время для
+const getFullTimeDay = (start, end, meetingStart, durationMeetingTime) => {
+  let result = false;
+  start = start.split(':');
+  end = end.split(':');
+  meetingStart = meetingStart.split(':');
 
-// *** функция палиндром //
-const isPalindromeString = (string) => {
-  string = string.replaceAll(' ', '').toLowerCase();
+  start = getHoursInMinutes(start);
+  end = getHoursInMinutes(end);
+  meetingStart = getHoursInMinutes(meetingStart);
+  durationMeetingTime += meetingStart;
 
-  let reversion = '';
-
-  for (let i = string.length - 1; i >= 0; i--) {
-    reversion += string[i];
-  }
-
-  return string === reversion;
+  if (durationMeetingTime >= start && durationMeetingTime <= end){
+    result = true;
+  } return result;
 };
 
-// *** функция проверки //
-isPalindromeString('Лёша на полке клопа нашёл '); // true
+//проверка работы функции
+console.log(getFullTimeDay('08:00', '17:30', '14:00', 90));
+console.log(getFullTimeDay('8:0', '10:0', '8:0', 120));
+console.log(getFullTimeDay('08:00', '14:30', '14:00', 90));
+console.log(getFullTimeDay('14:00', '17:30', '08:0', 90));
+console.log(getFullTimeDay('8:00', '17:30', '08:00', 900));
+
+export {getHoursInMinutes, getFullTimeDay};
