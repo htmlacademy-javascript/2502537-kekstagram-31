8ad87757574a983281b443495e97fb6117a0e2a1
rumforweb <ebertzz90@gmail.com>
2024-03-14 22:40:45 +0300

    Отрисуй меня полностью

--- js/functions.js
+++ js/functions.js
@@ -22,10 +22,10 @@ const getFullTimeDay = (start, end, meetingStart, durationMeetingTime) => {
 };
 
 //проверка работы функции
-console.log(getFullTimeDay('08:00', '17:30', '14:00', 90));
-console.log(getFullTimeDay('8:0', '10:0', '8:0', 120));
-console.log(getFullTimeDay('08:00', '14:30', '14:00', 90));
-console.log(getFullTimeDay('14:00', '17:30', '08:0', 90));
-console.log(getFullTimeDay('8:00', '17:30', '08:00', 900));
+// console.log(getFullTimeDay('08:00', '17:30', '14:00', 90));
+// console.log(getFullTimeDay('8:0', '10:0', '8:0', 120));
+// console.log(getFullTimeDay('08:00', '14:30', '14:00', 90));
+// console.log(getFullTimeDay('14:00', '17:30', '08:0', 90));
+// console.log(getFullTimeDay('8:00', '17:30', '08:00', 900));
 
 export {getHoursInMinutes, getFullTimeDay};
--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-import {generateUserPhotoDescription} from './create-photo-details.js';
+import { generateUserPhotoDescription } from './create-photo-details.js';
+import { pictureElement, pictureContainer } from './picture-elements.js';
+
 generateUserPhotoDescription();
+pictureElement();
+pictureContainer();
