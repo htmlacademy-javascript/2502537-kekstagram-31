feeb40bbda213b84815c15d26554bbe595cacf82
Keks <keks@htmlacademy.ru>
2024-03-23 18:32:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+import './data.js';
+import './render-big-picture.js';
+import './render.js';
+import './util.js';
---
+++ 8/js/util.js
@@ -0,0 +1,28 @@
+// функция генерации случайнего числа
+const getRandomNum = (min, max) => {
+  const reduce = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const high = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (high - reduce + 1) + reduce;
+  return Math.floor(result);
+};
+
+// функция генерации случайнего числа id
+const createRandomId = (min, max) => {
+  const earlyValues = [];
+
+  return () => {
+    let currentValue = getRandomNum(min, max);
+    if (earlyValues.length >= max - min + 1) {
+      // console.error(`Перебор всех чисел от ${min} до ${max}`);
+      return null;
+    }
+    while (earlyValues.includes(currentValue)) {
+      currentValue = getRandomNum(min, max);
+    }
+    earlyValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+getRandomNum();
+createRandomId();
